503e709da4bccb2ce7826c9dbc2ab6bde2da3b02
anniedelgee <117107188+anniedelgee@users.noreply.github.com>
2023-03-20 14:05:42 +0000

    imported inquirer package, in process of restructuring prompts

--- 11-es6-module/04-readme-generator-lesson/challenge/starter/index.js
+++ 11-es6-module/04-readme-generator-lesson/challenge/starter/index.js
@@ -5,77 +5,77 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 //and array of objects
-const questions = [
-    {
-        type: 'input',
-        message: 'Enter your github useraname',
-        name: 'username',
-        default: 'Username',
-        validate: (answer) => {
-            if (answer.length <1 ) {
-                return console.log("A valid github username is required.");
-            }
-            return true;
-        }
-    },
-    {
-        type:'input',
-        message: "Enter the name of your github repo",
-        name: 'repo',
-        default: 'Repository Title',
-        validate: (answer) => {
-            if (answer.length < 1) {
-                return console.log(" A valid github repo is required")
-            }
-            return true;
-        }
-    },
-    {
-        type:'input',
-        message: "Enter the title of your project",
-        name: 'title',
-        default: 'Project Title',
-        validate: (answer) => {
-            if (answer.length < 1) {
-                return console.log(" Your project currently has no title")
-            }
-            return true;
-        }
-    },
-    {
-        type:'input',
-        message: "Describe your project",
-        name: 'abstract',
-        default: 'Abstract',
-        validate: (answer) => {
-            if (answer.length < 1) {
-                return console.log(" A valid project description is required")
-            }
-            return true;
-        }
-    },
-    {
-        type: 'input',
-        message: 'Describe the installation process of your application',
-        name: 'installtion'
-    },
-    {
-        type: 'input',
-        message: 'Describe the best way to use your application',
-        name: 'usage'
-    },
-    {
-        type: 'input',
-        message: 'Credit collaborators or references used int he making of this project',
-        name: 'credits'
-    },
-    {
-        type: 'input',
-        message: 'Choose a license for your application',
-        choice: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-        name: 'license'
-    }
-];
+// const questions = [
+//     {
+//         type: 'input',
+//         message: 'Enter your github useraname',
+//         name: 'username',
+//         default: 'Username',
+//         validate: (answer) => {
+//             if (answer.length <1 ) {
+//                 return console.log("A valid github username is required.");
+//             }
+//             return true;
+//         }
+//     },
+//     {
+//         type:'input',
+//         message: "Enter the name of your github repo",
+//         name: 'repo',
+//         default: 'Repository Title',
+//         validate: (answer) => {
+//             if (answer.length < 1) {
+//                 return console.log(" A valid github repo is required")
+//             }
+//             return true;
+//         }
+//     },
+//     {
+//         type:'input',
+//         message: "Enter the title of your project",
+//         name: 'title',
+//         default: 'Project Title',
+//         validate: (answer) => {
+//             if (answer.length < 1) {
+//                 return console.log(" Your project currently has no title")
+//             }
+//             return true;
+//         }
+//     },
+//     {
+//         type:'input',
+//         message: "Describe your project",
+//         name: 'abstract',
+//         default: 'Abstract',
+//         validate: (answer) => {
+//             if (answer.length < 1) {
+//                 return console.log(" A valid project description is required")
+//             }
+//             return true;
+//         }
+//     },
+//     {
+//         type: 'input',
+//         message: 'Describe the installation process of your application',
+//         name: 'installtion'
+//     },
+//     {
+//         type: 'input',
+//         message: 'Describe the best way to use your application',
+//         name: 'usage'
+//     },
+//     {
+//         type: 'input',
+//         message: 'Credit collaborators or references used int he making of this project',
+//         name: 'credits'
+//     },
+//     {
+//         type: 'input',
+//         message: 'Choose a license for your application',
+//         choice: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+//         name: 'license'
+//     }
+// ];
 
 // function to write README file
 function writeToFile(fileName, data) {
