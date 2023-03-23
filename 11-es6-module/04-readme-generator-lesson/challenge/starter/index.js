05a31a7fec19f5b67eb16a6921caf4b9fd263c7c
anniedelgee <117107188+anniedelgee@users.noreply.github.com>
2023-03-23 11:13:05 +0000

    couldnt get node working

--- 11-es6-module/04-readme-generator-lesson/challenge/starter/index.js
+++ 11-es6-module/04-readme-generator-lesson/challenge/starter/index.js
@@ -1,91 +1,119 @@
+
+//external packages 
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
+const util= require('util');
+
+//internal packages
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-//and array of objects
-// const questions = [
-//     {
-//         type: 'input',
-//         message: 'Enter your github useraname',
-//         name: 'username',
-//         default: 'Username',
-//         validate: (answer) => {
-//             if (answer.length <1 ) {
-//                 return console.log("A valid github username is required.");
-//             }
-//             return true;
-//         }
-//     },
-//     {
-//         type:'input',
-//         message: "Enter the name of your github repo",
-//         name: 'repo',
-//         default: 'Repository Title',
-//         validate: (answer) => {
-//             if (answer.length < 1) {
-//                 return console.log(" A valid github repo is required")
-//             }
-//             return true;
-//         }
-//     },
-//     {
-//         type:'input',
-//         message: "Enter the title of your project",
-//         name: 'title',
-//         default: 'Project Title',
-//         validate: (answer) => {
-//             if (answer.length < 1) {
-//                 return console.log(" Your project currently has no title")
-//             }
-//             return true;
-//         }
-//     },
-//     {
-//         type:'input',
-//         message: "Describe your project",
-//         name: 'abstract',
-//         default: 'Abstract',
-//         validate: (answer) => {
-//             if (answer.length < 1) {
-//                 return console.log(" A valid project description is required")
-//             }
-//             return true;
-//         }
-//     },
-//     {
-//         type: 'input',
-//         message: 'Describe the installation process of your application',
-//         name: 'installtion'
-//     },
-//     {
-//         type: 'input',
-//         message: 'Describe the best way to use your application',
-//         name: 'usage'
-//     },
-//     {
-//         type: 'input',
-//         message: 'Credit collaborators or references used int he making of this project',
-//         name: 'credits'
-//     },
-//     {
-//         type: 'input',
-//         message: 'Choose a license for your application',
-//         choice: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-//         name: 'license'
-//     }
-// ];
+// break down ReadMe sections into objects in array.
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter your github useraname',
+        name: 'username',
+        default: 'Username',
+        validate: (answer) => {
+            if (answer.length <1 ) {
+                return console.log("A valid github username is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type:'input',
+        message: "Enter the name of your github repo title",
+        name: 'repo',
+        default: 'Repository Title',
+        validate: (answer) => {
+            if (answer.length < 1) {
+                return console.log(" A valid github repo is required")
+            }
+            return true;
+        }
+    },
+    {
+        type:'input',
+        message: "Enter the title of your project",
+        name: 'title',
+        default: 'Project Title',
+        validate: (answer) => {
+            if (answer.length < 1) {
+                return console.log(" Your project currently has no title")
+            }
+            return true;
+        }
+    },
+    {
+        type:'input',
+        message: "Describe your project",
+        name: 'abstract',
+        default: 'Abstract',
+        validate: (answer) => {
+            if (answer.length < 1) {
+                return console.log(" A valid project description is required")
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: 'Describe the installation process of your application',
+        name: 'installtion'
+    },
+    {
+        type: 'input',
+        message: 'Describe the best way to use your application',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Give instructions for how to contribute to your applciation',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'Credit collaborators or references used int he making of this project',
+        name: 'credits'
+    },
+    {
+        type: 'input',
+        message: 'GIve intructions for testing the programme/any tests written for it',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'Choose a license for your application',
+        choice: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    }
+];
 
 // function to write README file
+// write ternary oeprator for writing file or throwing error
 function writeToFile(fileName, data) {
+    fs.writeToFile(fileName, data);
+    error ? console.log(err) : console.log("your newly generated readMe awaits!");
 
 }
 
+const writeFileAsync = util.promisify(writeToFile);
+
 // function to initialize program
-function init() {
+//use inquirer prompt function 
+init= () => {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        const newReadMe = generateMarkdown(answers)
+    })
 
 }
 
 // function call to initialize program
 init();
+
+
+
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # layzeeCoder-readMe-generator
 
 ## Abstract
-A good readMe file goes a long way in getting the code community interested in your project. This application uses ES6 and node.js to generates a readMe file based on user input. 
+A good readMe file goes a long way in getting the code community interested in your project. But this also takes some time out of the project schedule that could otherwise be spent building the application itself. This application uses ES6 and node.js to generates a readMe file based on user input. 
 
 ## Application Screenshot
 
